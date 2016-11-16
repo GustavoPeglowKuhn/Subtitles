f453968c5501f2b532da1a3c736498f4c2f7c6e2
GustavoKuhn <gustavowolgep@gmail.com>
2016-11-16 03:04:03 +0000

    V 1.0.4

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-	Esse programa é uma ferramenta para ajustar o tempo das legendas de filmes
+Esse programa é uma ferramenta para ajustar o tempo das legendas de filmes
 de maneira facil, apenas inserindo um tempo de adiantamento ou atrazo, todo o
 arquivo str tera seu tempo ajustado, sem a necessidade de modificar tudo
 manualmente ou baixar outra versão da legenda.
 
-	Como utilizar: Altere o programa padrao para abrir arquivos de legenda
+	Como utilizar:
+Altere o programa padrao para abrir arquivos de legenda
 (.srt) para a ferammenta Subtitles, assim basta abrir o arquivo e digitar o 
 delay. Se estiver usando windows, a ferramenta pode ser adicionada as variaveis
 de ambiente PATH e executada diretamente pelo terminal com:
 "Subtitles arquivo.srt delay", sendo o delay em milisegundos.
 
-	Como alterar o programa padrao: clique com o botao direito do mouse sobre um
+	Como alterar o programa padrao:
+Clique com o botao direito do mouse sobre um
 arquivo .srt e va em "abrir com -> escolher outro aplicativo", depois clique em
 "mais aplicativos -> procurar por outro aplicativo neste PC" e indique o local 
 do arquivo "Subtitles.exe", procure em
 "Downloads\Subtitles\bin\Release\Subtitles.exe".
 
-	Desenvolvedores: o projeto foi feito usando o software Code::Blocks, para
+	Desenvolvedores:
+O projeto foi feito usando o software Code::Blocks, para
 edita-lo abra o arquivo "Subtitles.cbp"
 
-	
 
 por Alemao
 may the Force be with you
---
+++ mensagens.h
@@ -0,0 +1,32 @@
+#ifndef _MENSAGENS_H
+#define _MENSAGENS_H
+
+#define LOAD_ERROR1 "erro ao abrir "
+#define LOAD_ERROR2 "verifique a ortografia, evite acentos e espaços\n(nao esqueca de inserir a extensão do arquivo, geralmente \".srt\")"
+#define LOAD_OK "importado com sucesso!"
+
+#define SAVE_ERROR1 "erro ao salvar "
+#define SAVE_ERROR2 "verifique a ortografia, evite acentos e espaços"
+#define SAVE_ERROR3 "verifique se o arquivo é somente leitura"
+#define SAVE_OK "salvo com sucesso"
+
+#define FILE_INFO1 "o arquivo possui: "
+#define FILE_INFO2 " itens de legenda."
+
+#define ADIANTADO " ms adiantado"
+#define ATRAZADO  " ms atrazado"
+
+#define ASK_FILE "digite o nome do arquivo: "
+#define ASK_DELAY "digite o tempo de atrazo, em milisegundos, desejado:\n(para atrazar a legenda entre com um numero negativo)"
+
+#define HELP "Para alterar uma legenda digite no terminal \"Subtitles arquivo delay\"\nsendo arquivo o caminho completo, ou relativo a pasta em que o terminal esta,\ninclua a extenxao do arquivo, geralmente .srt, e delay é o tempo, em\nmilisegundos, que a legenda deve ser adiantada ou atrazada.\n\nOutra maneira de utilizar o aplicativo e modificar o aplicativo padrao para \nabrir arquivos .srt\n\nCaso tenha erro ao abrir o arquivo verifique se o arquivo possui acentos,\nespacos ou caracteres especiais, remova-os e tente novamente. Se o erro\npercistiro verifique encoding do arquivo, recomendamos que use UTF-8.\n\nCaso tenha erro ao salvar o arquivo verifique se o arquivo é somente leitura, va\nem propriedades e desmarque a caixa \"somente leitura\".\n\nmais duvidas, sugestoes, ou patrocinios a este projetos entre em contato com o\ndesenvolvedor."
+
+//ASCII ART
+#define VIKING "                         ______|\\\n                    _.--|alemao|:\n                   <____|..----||\n                          .---''---,\n                            ;..__..'    _...\n                          ,'/  ;|/..--''    \\\n                        ,'_/.-/':            :\n                   _..-'''/  /  |  \\    \\   _|/|\n                  \\      /-./_ \\;   \\    \\,;'   \\\n                  ,\\    / \\:  `:\\    \\   //    `:`.\n                ,'  \\  /-._;   | :    : ::    ,.   .\n              ,'     ::   /`-._| |    | || ' :  `.`.)\n           _,'       |;._:: |  | |    | `|   :    `'\n         ,'   `.     /   |`-:_ ; |    |  |  : \\\n         `--.   )   /|-._:    :          |   \\ \\\n            /  /   :_|   ;`-._;   __..--';    : :\n           /  (    ;|;-./_  _/.-:'o |   /     ' |\n          /  , \\._/_/_./--''/_|:|___|_,'        |\n         :  /   `'-'--'----'---------'          |\n         | :     O ._O   O_. O ._O   O_.      ; ;\n         : `.      //    //    //    //     ,' /\n       ~~~`.______//____//____//____//_______,'~\n                 //    //~   //    //\n          ~~   _//   _//   _// ~ _//     ~\n        ~     / /   / /   / /   / /  ~      ~~\n             ~~~   ~~~   ~~~   ~~~"
+#define HAND "           _,-,-.\n        ,-: |.'-:|..-.\n   _..-:| | `-`. \\.--'\n <...--:'-|_|_|;  \\\n        \\   /     /\n         :      ,'\n         |      |"
+#define SCROLL "	,--.,---..._,-.\n	\\`--|~~~    \\-':\n	 :-.| ~~ ~~~ : |\n	 |  |~~ ~~~  |,|\n	 |  |~ ~~ ~~ | |\n	 ; _|~~~ ~~  ; |\n	/   ; ~~  ~ /` |\n	`--'-.._____>..'"
+#define RUNAS "  ,     .   .    .    .   .   , .      .   .    .   .   .  , .     ,  \n,' `,   |   |\\   |`.  |`. |  /  |`.    |   |`.  |`. |`. ,'`. |`. ,' `,\n `,'   ,|.  |,'  |  | |._ |,' . |  |  ,|.  |  | |._ |._ `.,' |,'  `,' \n,' `. / | \\ | `. |  | |   |   ' |  | / | \\ |  | |   |   ,'`. |,' ,' `."
+#define THOR "    .        ,      .\n    |`,    ,' `,    |\\\n    |'      `,'     |,'\n    |      ,' `.    | `."
+
+
+#endif // _MENSAGENS_H
